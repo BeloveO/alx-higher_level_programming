4944419a16311feb682d4abdb66eb4edd3bd78c2
Belove <beloveolusola@gmail.com>
2023-05-10 05:20:27 +0100

    Print alphabets in reverse and alternating order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in range(90, 64, -1):
     if a % 2 == 0:
-        print(chr(a + 32), end='')
+        print("{:c}".format(a + 32), end='')
     else:
-        print(chr(a), end='')
+        print("{:c}".format(a), end='')
