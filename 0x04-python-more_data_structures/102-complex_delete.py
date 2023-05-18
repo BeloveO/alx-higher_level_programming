c27ddae7cf5171887150fd4a81a9235172fe0c43
Belove <beloveolusola@gmail.com>
2023-05-18 04:51:01 +0100

    Fixed some errors

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,8 +4,8 @@ def complex_delete(a_dictionary, value):
     for k, v in a_dictionary.items():
         if v == value:
             keys.append(k)
-    
+
     for k in keys:
         del a_dictionary[k]
-    
+
     return (a_dictionary)
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,4 +4,4 @@ def multiply_by_2(a_dictionary):
     for key, value in a_dictionary.items():
         val = value * 2
         new[key] = val
-    return(new)
+    return (new)
