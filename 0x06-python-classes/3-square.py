605765a3e3b7945a3cd6f0022ae33792601f78f8
Belove <beloveolusola@gmail.com>
2023-05-31 03:38:33 +0100

    Public inst method added

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+Define class Square
+
+"""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """ Method to initialize the square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
+
+
+def area(self):
+    """ Method that returns the area of the square"""
+    return (self.__size ** 2)
