17e52f01aa67b703c5d707c111a0d940054d664c
Belove <beloveolusola@gmail.com>
2023-05-31 04:02:06 +0100

    Prints a # square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+
+Define class Square
+
+"""
+
+
+class Square:
+    """Represent a square."""
+    @property
+    def size(self):
+        """ Get/set the current size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Method to set the size value of the square object."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(value)
+
+    def area(self):
+        """ Method that returns the area of the square object
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """ Method that prints a # square according to the size value
+        """
+        if not self.__size:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
+
+    def __init__(self, size=0):
+        """ Method to initialize the square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
