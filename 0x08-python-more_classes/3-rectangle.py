c86fbcc2ce3d57ff2ec97dc89aca92ff048bc016
Belove <beloveolusola@gmail.com>
2023-06-06 01:51:03 +0100

    Printing out a rectangle with special characters

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+A module with a rctangle that does nothing
+"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Get the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        """Init a new rectangle."""
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Returns the perimter of the rectangle."""
+        if self.__height == 0 or self.__width == 0:
+            return (0)
+        return ((self.__height * 2) + (self.__width * 2))
+
+    def __str__(self):
+        """Returns printable rectangle in # character"""
+        rect = ""
+        if self.__height == 0 or self.__width == 0:
+            return rect
+        for i in range(self.__height):
+            rect += ("#" * self.__width) + "\n"
+        return rect[:-1]
