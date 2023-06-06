88cc67e108df55188c7668919193e9a52639f974
Belove <beloveolusola@gmail.com>
2023-06-06 01:26:49 +0100

    Adding new parameters to rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A module with a rctangle that does nothing
+"""
+class Rectangle:
+    """Defines a rectangle"""
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """Get the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    def __init__(self, width=0, height=0):
+        """Init a new rectangle."""
+        self.width = width
+        self.height = height
