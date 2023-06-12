b2e4965db2e74afef1ae30a15641f2e6678f2afe
Belove <beloveolusola@gmail.com>
2023-06-12 03:50:51 +0100

    Check for isinstance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function to check for isinstance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if obj is an instance of a_class
+    """
+    return isinstance(obj, a_class)
