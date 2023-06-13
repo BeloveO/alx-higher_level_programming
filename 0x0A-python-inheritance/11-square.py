6eef9a02f9b413deaba4a5a694d92680acecc3d3
Belove <beloveolusola@gmail.com>
2023-06-13 02:21:49 +0100

    Adding more methods to the inheritance class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Defines a class Square that inherits from the class Rectangle.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class to define square from Rectangle class
+    """
+    def __init__(self, size):
+        """Initializes instances"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Method to return a string with the area"""
+        return super().area()
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
