e638e83de16ac2b6bcf8ed539503658fc9f4cd6a
Belove <beloveolusola@gmail.com>
2023-06-13 02:27:31 +0100

    Inheritance with inverted operators

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Class MyInt that inherits from int
+"""
+
+
+class MyInt(int):
+    """
+    Methods that return == and != are inverted
+    """
+    def __eq__(self, other):
+        return int.__ne__(self, other)
+    def __ne__(self, other):
+        return int.__eq__(self, other)
