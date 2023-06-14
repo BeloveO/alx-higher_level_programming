e63c499ba8f88ae83c1dced2f02a02a3df32174f
Belove <beloveolusola@gmail.com>
2023-06-14 03:52:30 +0100

    Appending after special lines

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Function that inserts a line of text after some special characters
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    res = []
+    with open(filename, 'r', encoding="utf-8") as file:
+        for line in file:
+            res += [line]
+            if line.find(search_string) != -1:
+                res += [new_string]
+    with open(filename, 'w', encoding="utf-8") as file:
+        file.write("".join(res))
