a609fd7d49a6cc07c29d3dc7fcf7dc411cb65d37
Belove <beloveolusola@gmail.com>
2023-06-14 03:54:00 +0100

    Appending after some lines

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,6 +5,9 @@ Function that inserts a line of text after some special characters
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """
+    function that inserts a line of text to a file, after each line containing a specific string
+    """
     res = []
     with open(filename, 'r', encoding="utf-8") as file:
         for line in file:
