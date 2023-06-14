0889fc97027f71f0e1625b06cbff469317e7f08d
Belove <beloveolusola@gmail.com>
2023-06-14 02:07:14 +0100

    Json representation of a string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+Function to return JSON rep of an object
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of a string
+    """
+    return json.dumps(my_obj)
