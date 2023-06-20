1f4d32c620ef347380b912f50941abb5e57b085e
Belove <beloveolusola@gmail.com>
2023-06-20 02:00:43 +0100

    Update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,3 +28,23 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Update method
+        """
+        if args is not None and len(args) is not 0:
+            list_attr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_attr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_attr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
