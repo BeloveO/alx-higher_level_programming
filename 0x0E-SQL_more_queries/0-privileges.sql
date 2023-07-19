e9b6f539ab35f0a602acfc53b94c61b9b2f8c12a
BeloveO <beloveolusola@gmail.com>
2023-07-19 16:47:17 -0700

    Showing privileges granted

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Lists all priviledges of users on the database
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Lists all privileges granted to a user on the database
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
