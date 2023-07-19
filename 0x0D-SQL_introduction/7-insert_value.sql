6cd6a46c48a51fab7f1347dabf34b3eebf234c55
BeloveO <beloveolusola@gmail.com>
2023-07-18 21:53:44 -0700

    adding and counting new rows

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Inserts new rows in the table
+INSERT INTO first_table VALUES(89, 'Best School');
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Displays the number of id with 89 as value
+SELECT COUNT(*)
+FROM first_table
+WHERE id = 89;
