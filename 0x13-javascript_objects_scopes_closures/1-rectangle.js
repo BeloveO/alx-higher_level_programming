6af5de219cd3bf151d0f2920e205c8ecc311f2d7
Belove <beloveolusola@gmail.com>
2023-08-16 17:09:03 +0100

    Rectangle and parameters

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Write a class Rectangle that defines a rectangle with its parameters.
 
-modules.export = class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
-};
+}
