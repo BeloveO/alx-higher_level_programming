e9a48e1f3cc4543242de2f1538b84bb3ae8f1802
Belove <beloveolusola@gmail.com>
2023-08-16 17:39:50 +0100

    Rectangle methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+    constructor (w, h) {
+      if ((w > 0) && (h > 0)) {
+        this.width = w;
+        this.height = h;
+      } 
+    }
+
+    print () {
+        for (let i = 0; i < this.height; ++i) {
+            let s = '';
+            
+            for (let j = 0; j < this.width; ++j) {
+                s += 'X';
+            }
+            console.log(s);
+        } 
+    }
+
+    rotate () {
+        const alt = this.width;
+        this.width = this.height;
+        this.height = alt;
+    }
+
+    double () {
+        this.height *= 2;
+        this.width *= 2;
+    }
+};
