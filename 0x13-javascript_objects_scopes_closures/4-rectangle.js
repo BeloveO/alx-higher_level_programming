e10a1bed9a9cd7cef64905dd4db93ff825991ea2
BeloveO <beloveolusola@gmail.com>
2023-08-17 03:18:14 +0000

    Fixed errors

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,32 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if ((w > 0) && (h > 0)) {
-        this.width = w;
-        this.height = h;
-      } 
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print () {
-        for (let i = 0; i < this.height; ++i) {
-            let s = '';
-            
-            for (let j = 0; j < this.width; ++j) {
-                s += 'X';
-            }
-            console.log(s);
-        } 
-    }
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      let s = '';
 
-    rotate () {
-        const alt = this.width;
-        this.width = this.height;
-        this.height = alt;
+      for (let j = 0; j < this.width; ++j) {
+        s += 'X';
+      }
+      console.log(s);
     }
+  }
 
-    double () {
-        this.height *= 2;
-        this.width *= 2;
-    }
+  rotate () {
+    const alt = this.width;
+    this.width = this.height;
+    this.height = alt;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
 };
