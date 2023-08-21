166ad21e40a4efd1b02f8f7a3011b28a0b6911a4
Belove <beloveolusola@gmail.com>
2023-08-21 04:23:07 +0100

    City

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a python file that contains the class definition of a 
+State and an instance Base = declarative_base():
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class City(Base):
+    """
+    links to the MySQL table cities, class attribute id, 
+    name and state_id(foreign key to states.id)
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states_id"), nullable=False)
