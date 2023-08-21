2d9ded292e9acf16621aacae6e0f6ebe29baf346
Belove <beloveolusola@gmail.com>
2023-08-21 02:58:34 +0100

    States starting with N

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,16 +4,16 @@ This script lists all states from the
 database `hbtn_0e_0_usa`.
 """
 
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
                          passwd=sys.argv[2], db=sys.argv[3])
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    states = cur.fetchall()
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    states = c.fetchall()
 
     for state in states:
         print(state)
