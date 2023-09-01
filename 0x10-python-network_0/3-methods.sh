69f9cb1587583fb71db3e289708fd96aa6f02c44
Belove <beloveolusola@gmail.com>
2023-09-01 03:03:01 +0100

    http

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take in a URL and displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$1" |GREP "Allow:" | -d " " -f 2-
+curl -sI -X OPTIONS "$1" | grep "Allow:" | -d " " -f 2-
