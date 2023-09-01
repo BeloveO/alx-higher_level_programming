ec747f0671ec1751d582f2a1b64d090fcb698eac
Belove <beloveolusola@gmail.com>
2023-09-01 03:45:19 +0100

    Specified message

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to a port, cause response with specified message
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin: You got me!" -d "user_id=98"
+curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:You got me!"
