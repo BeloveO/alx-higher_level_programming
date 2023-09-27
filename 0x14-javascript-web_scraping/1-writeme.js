19e2a683e85237226d7ba2bfa514fab9b0280786
Belove <beloveolusola@gmail.com>
2023-09-27 04:51:40 +0100

    Webspage storage

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3], 'utf-8', function (err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
